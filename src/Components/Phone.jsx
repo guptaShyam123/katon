573285980b52a6522206dad62ad254bc423b02c3
Shyam gupta <shyam.gupta@iskylar.in>
2022-07-28 17:16:32 +0530

    first commit

--- src/Components/Phone.jsx
+++ src/Components/Phone.jsx
@@ -2,14 +2,16 @@ import React from 'react'
 import Img from '../assets/Photos/baby.png'
 import imge from '../assets/Photos/color.png'
 import { styled } from "@mui/system";
-import { AppBar } from "@mui/material"
-import { useTheme } from "@mui/material/styles";
+
+
 import photo from '../assets/Photos/111.svg'
+import blue from '../assets/Photos/bluebanner.svg'
 
 const Phone = () => {
   return (
     <>
     <img style={{width:'100%'}} src={photo} />
+ //   <img style={{width:'100%'}} src={blue}/>
     </>
   )
 }
